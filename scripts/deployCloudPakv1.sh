60324e01771d5414184ce1fe67b9343b9bf66f1c
amiivas <amitsrikiet@gmail.com>
2021-02-21 00:11:58 +0530

    Update deployCloudPakv1.sh

--- scripts/deployCloudPakv1.sh
+++ scripts/deployCloudPakv1.sh
@@ -480,8 +480,8 @@ EOF
   then
     echo "INFO: Installing Runtime MQ";
     echo "Product Installation Path: ${productInstallationPath}"
-    curl ${productInstallationPath}/install-mq.sh -o install-mq.sh
-    sh install-mq.sh ${CLUSTERNAME} ${DOMAINNAME} ${OPENSHIFTUSER} ${OPENSHIFTPASSWORD} ${namespace}
+    #curl ${productInstallationPath}/install-mq.sh -o install-mq.sh
+    sh ./product_scripts/install-mq.sh ${CLUSTERNAME} ${DOMAINNAME} ${OPENSHIFTUSER} ${OPENSHIFTPASSWORD} ${namespace}
     #wait_for_product QueueManager mq
   fi
   
