e767cc4ea6855c3cd4bf8f2648e383b890f7be57
amiivas <amitsrikiet@gmail.com>
2021-03-14 13:04:29 +0530

    Update publishProducts.sh

--- scripts/product_scripts/apic/publishProducts.sh
+++ scripts/product_scripts/apic/publishProducts.sh
@@ -23,7 +23,7 @@ echo "Logging to API Manager :: ${apic_server} for user ${user} and password ${p
 
 apic login --server ${apic_server} --username ${user} --password ${password} --realm provider/default-idp-2
 
-sleep 2
+sleep 5
 products_folder_path="./products/"
 cd ${products_folder_path}
  
@@ -32,7 +32,7 @@ for FILE in *product*;
 do 
    if [[ -f "$FILE" ]]; then
      echo  "Publishing $FILE"
-     apic products:publish --server ${apic_server} --org ${org} --scope catalog --catalog sandbox $FILE
+     apic products:publish --server ${apic_server} --org ${org} --catalog sandbox --accept-license --live-help --debug cts-demo-apic-product_1.0.0.yaml
      var=$?
      
      if [[ var -eq 0 ]]; then
@@ -43,20 +43,20 @@ do
      echo "No Products to publish !!."
    fi
 done
-sleep 5
-echo "Uploading APIs in draft state in API Manager"
-for FILE in *; 
-do 
-   if [[ -f "$FILE" ]]; then
-     echo  "Uploading $FILE"
-     apic draft-apis:create --server ${apic_server} --org ${org} $FILE
-     var=$?
-     if [[ var -eq 0 ]]; then
-       mkdir -p ../draftapis
-       mv $FILE ../draftapis/.
-     fi
-   else 
-     echo "No APIs to upload !!."
-   fi
-done
+#sleep 5
+#echo "Uploading APIs in draft state in API Manager"
+#for FILE in *; 
+#do 
+#   if [[ -f "$FILE" ]]; then
+#     echo  "Uploading $FILE"
+#     apic draft-apis:create --server ${apic_server} --org ${org} $FILE
+#     var=$?
+#     if [[ var -eq 0 ]]; then
+#       mkdir -p ../draftapis
+#       mv $FILE ../draftapis/.
+#     fi
+#   else 
+#     echo "No APIs to upload !!."
+#   fi
+#done
 
